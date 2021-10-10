29e113d9d0ca497af444fb2e41fde82edcc7245a
JDhyeok <jdhyeok97@gmail.com>
2021-10-10 17:12:20 +0900

    Refactor predict api

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/api')
 def hello():
     """
-    api test
+    test
     """
     return "Hello World"
 
@@ -21,57 +21,60 @@ def predict():
     """
     Predict sentiment of movie review
     """
+    pred = []
 
-    data = {"success": False}
     params = request.json
-    score = 0
 
     if(params == None):
         params = request.args
 
     if(params != None):
-        data["response"] = params.get("msg")
-        data["success"] = True
-
         print(params['contents'])
 
-        score = sentiment_predict(params['contents'])
+        pad_new = sentiment_predict(params['contents'])
+        score = float(model.predict(pad_new))
 
-    return jsonify(str(score))
+        pred.append("Positive" if score > 0.5 else "Negative")
+        pred.append(score * 100 if score > 0.5 else (1 - score) * 100)
+
+    return jsonify(
+        sentiment=pred[0],
+        score=pred[1]
+    )
+
+
+def to_lowercase(sentence):
+    sentence = re.sub('[^0-9a-zA-Z ]', '', sentence).lower()
+    return sentence
+
+
+def encoding(sentence):
+    encoded = []
+
+    for word in sentence.split():
+        try:
+            if word_to_index[word] <= 1000:
+                encoded.append(word_to_index[word] + 3)
+            else:
+                encoded.append(2)
+        except KeyError:
+            encoded.append(2)
+
+    return encoded
 
 
 def sentiment_predict(sentence):
     """
-    
+    text preprocessing
     """
-    # 알파벳과 숫자를 제외하고 모두 제거 및 알파벳 소문자화
-    sentence = re.sub('[^0-9a-zA-Z ]', '', sentence).lower()
-
-    # 정수 인코딩
-    encoded = []
-    for word in sentence.split():
-        # 단어 집합의 크기를 10,000으로 제한.
-        try:
-            if word_to_index[word] <= 10000:
-                encoded.append(word_to_index[word]+3)
-            else:
-                # 10,000 이상의 숫자는 <unk> 토큰으로 취급.
-                encoded.append(2)
-            # 단어 집합에 없는 단어는 <unk> 토큰으로 취급.
-        except KeyError:
-            encoded.append(2)
-
-    pad_new = pad_sequences([encoded], maxlen=max_len)  # 패딩
-    score = float(model.predict(pad_new))  # 예측
     pred = []
-    pred.append("Positive" if score > 0.5 else "Negative")
-    pred.append(score * 100 if score > 0.5 else (1 - score) * 100)
 
-    return pred
+    sentence = to_lowercase(sentence)
+    encoded = encoding(sentence)
+    pad_new = pad_sequences([encoded], maxlen=max_len)  # 패딩
+
+    return pad_new
 
 
 if __name__ == "__main__":
-    """
-    start flask server
-    """
     app.run(debug=True)
--- app/static.py
+++ app/static.py
@@ -2,7 +2,6 @@ from tensorflow import keras
 
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 model = keras.models.load_model('GRU_model.h5')
 word_to_index = imdb.get_word_index()
